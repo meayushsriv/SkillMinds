02e93110d8f4dbce8362334b34fb21073617f914
Factve <me.facteverse@gmail.com>
2024-06-01 23:09:57 +0530

    fifth commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,11 @@ import Campus from './components/Campus/Campus';
 import Testimonials from './components/Testimonials/Testimonials';
 import Contact from './components/Contact/Contact';
 import Footer from './components/Footer/Footer';
+import VideoPlayer from './components/VideoPlayer/VideoPlayer';
+import { useState } from 'react';
 
 function App() {
+  const [playState, setPlayState]=useState(false);
   return (
     <div className="App">
       <Navbar />
@@ -16,7 +19,7 @@ function App() {
       <div className='container'>
         <Title subTitle='Our PROGRAM' title='What We Offer' />
         <Programs />
-        <About />
+        <About setPlayState={setPlayState} />
         <Title subTitle='Gallery' title='Campus Photos' />
         <Campus />
         <Title subTitle='Testimonials' title='What Student Says' />
@@ -25,6 +28,7 @@ function App() {
         <Contact />
         <Footer />
       </div>
+      <VideoPlayer playState={playState} setPlayState={setPlayState} />
     </div>
   );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import './navbar.css';
 import logo from '../../assets/logo.png';
+import { Link } from "react-scroll";
+import menu from '../../assets/menu-icon.png';
 
 const Navbar = () => {
     const [sticky,setSticky]=useState(false);
@@ -9,18 +11,23 @@ const Navbar = () => {
             window.scrollY > 50 ? setSticky(true) : setSticky(false);
         })
     },[]);
+
+    const [mobileMenu, setMobileMenu]=useState(false);
+    const toggleMenu = () => {
+        mobileMenu ? setMobileMenu(false) : setMobileMenu(true);
+    }
     return (
         <nav className={`container ${sticky ? 'dark-nav' : ''}`}>
             <img className="logo" src={logo} alt="" />
-            <ul>
-                <li>Home</li>
-                <li>Program</li>
-                <li>About us</li>
-                <li>Campus</li>
-                <li>Testimonials</li>
-                <li><button className="btn">Contact us</button></li>
+            <ul className={mobileMenu ? '' : 'hide-mobile-menu'}>
+                <li><Link to="hero" smooth={true} offset={0} duration={500}>Home</Link></li>
+                <li><Link to="programs" smooth={true} offset={-260} duration={500}>Program</Link></li>
+                <li><Link to="about" smooth={true} offset={-130} duration={500}>About us</Link></li>
+                <li><Link to="campus" smooth={true} offset={-260} duration={500}>Campus</Link></li>
+                <li><Link to="testimonials" smooth={true} offset={-260} duration={500}>Testimonials</Link></li>
+                <li><Link to="contact" smooth={true} offset={-260} duration={500}>Contact us</Link></li>
             </ul>
-
+            <img src={menu} alt="" className="menu-icon" onClick={toggleMenu} />
         </nav>
     );
 }
