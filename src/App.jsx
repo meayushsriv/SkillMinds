05f5ecad4496c565a6b07390dddca5d74687dd59
Factve <me.facteverse@gmail.com>
2024-06-01 17:35:48 +0530

    fourth commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import Programs from './components/Programs/Programs';
 import Title from './components/Title/Title';
 import About from './components/About/About';
 import Campus from './components/Campus/Campus';
+import Testimonials from './components/Testimonials/Testimonials';
+import Contact from './components/Contact/Contact';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
@@ -16,6 +19,11 @@ function App() {
         <About />
         <Title subTitle='Gallery' title='Campus Photos' />
         <Campus />
+        <Title subTitle='Testimonials' title='What Student Says' />
+        <Testimonials />
+        <Title subTitle='Apply For Internship' title='Get in Touch' />
+        <Contact />
+        <Footer />
       </div>
     </div>
   );
